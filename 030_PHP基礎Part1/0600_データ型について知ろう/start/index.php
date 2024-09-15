28f2ad5dbfe5a5ff11636ddb013c1c614daf18c8
ohashi-yohei@fujibuturyu.co.jp <hashi@MacBook-Pro-2.local>
2024-09-15 19:56:45 +0900

    データ型について知ろう

--- 030_PHP基礎Part1/0600_データ型について知ろう/start/index.php
+++ 030_PHP基礎Part1/0600_データ型について知ろう/start/index.php
@@ -1,1 +1,13 @@
-<?php 
+<?php
+// データ型とは？
+$i = 1;
+$b = true;
+$str = 'hello';
+var_dump($b);
+// データ型の確認方法 var_dump()
+
+// 異なる型は自動的に変換される。
+// echo $i + (int) $b;
+
+// 型の取り扱いの注意点
+var_dump($i === '1');
