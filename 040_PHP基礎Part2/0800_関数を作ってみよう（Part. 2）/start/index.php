e402a8146a564e85db2253e543387d7d7d4283c4
ohashi-yohei@fujibuturyu.co.jp <hashi@MacBook-Pro-2.local>
2024-09-24 22:58:09 +0900

    0800_関数を作ってみよう（Part. 2）

--- 040_PHP基礎Part2/0800_関数を作ってみよう（Part. 2）/start/index.php
+++ 040_PHP基礎Part2/0800_関数を作ってみよう（Part. 2）/start/index.php
@@ -1,1 +1,13 @@
 <?php
+
+$price = 1000;
+
+function withTax($basePrice, $taxRate = 0.1)
+{
+    $sumPrice = $basePrice * (1 + $taxRate);
+    $sumPrice = round($sumPrice);
+    return number_format($sumPrice);
+}
+$fn = "withTax";
+$price = $fn($price, 0.08);
+echo $price . '円';
