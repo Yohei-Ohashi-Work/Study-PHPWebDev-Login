a3554697cde857bee63eca584599bd2b6a4eb5a5
ohashi-yohei@fujibuturyu.co.jp <hashi@MacBook-Pro-2.local>
2024-09-24 22:46:24 +0900

    0700_関数を作ってみよう（Part. 1）

--- 040_PHP基礎Part2/0700_関数を作ってみよう（Part. 1）/start/index.php
+++ 040_PHP基礎Part2/0700_関数を作ってみよう（Part. 1）/start/index.php
@@ -1,1 +1,16 @@
 <?php
+
+$numbers = [1, 2, 3, 4];
+$numbers2 = [1, 2, 3];
+
+function sum($numbers)
+{
+    $sum = 0;
+    foreach ($numbers as $number) {
+        $sum += $number;
+    }
+    return $sum;
+}
+
+echo "合計： " . sum($numbers) . "<br>";
+echo "合計： " . sum($numbers2) . "<br>";
