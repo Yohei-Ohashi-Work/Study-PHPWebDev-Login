807309471e1109e5b71c51398fe88aac40c41944
ohashi-yohei@fujibuturyu.co.jp <hashi@MacBook-Pro-2.local>
2024-09-24 23:07:42 +0900

    0900_PHPDocを書いてみよう

--- 040_PHP基礎Part2/0900_PHPDocを書いてみよう/start/index.php
+++ 040_PHP基礎Part2/0900_PHPDocを書いてみよう/start/index.php
@@ -1,6 +1,21 @@
 <?php
-function with_tax($base_price, $tax_rate = 0.1) {
+
+/**
+ * 税込金額を計算する関数
+ * 
+ * @param int $base_price 税抜金額
+ * @param float $tax_rate 税率
+ * @return int 税込金額
+ * @see https://example.com
+ * @author 大橋 庸平
+ * @version 1.0.0
+ */
+function with_tax($base_price, $tax_rate = 0.1)
+{
     $sum_price = $base_price + ($base_price * $tax_rate);
     $sum_price = round($sum_price);
     return $sum_price;
 }
+
+$price = with_tax(1000, 0.08);
+echo $price;
